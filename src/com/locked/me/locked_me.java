539a870288a12e6bf091e695b8551556dc0eb153
AutomationBoi <himesh8chopra@gmail.com>
2021-06-22 06:53:09 +0000

    frst commit

---
+++ src/com/locked/me/locked_me.java
@@ -0,0 +1,10 @@
+package com.locked.me;
+
+public class locked_me {
+	static locked_me_helperfunctions help_obj=new locked_me_helperfunctions();
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		help_obj.welcome_screen();
+	}
+
+}
---
+++ src/com/locked/me/locked_me_helperfunctions.java
@@ -0,0 +1,15 @@
+package com.locked.me;
+import java.io.*;
+
+
+public class locked_me_helperfunctions {
+	
+	public void welcome_screen() {
+		System.out.println("Locked ME File Handler");
+		System.out.println("-dev: Himesh Chopra");
+		System.out.println("Press \"l or L\" to add a file.");
+		System.out.println("Press \"d or D\" for further details");
+		System.out.println("Press \"a\" to add a file.");
+	}
+
+}
