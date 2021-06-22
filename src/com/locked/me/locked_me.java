1abbb5e1dadb64ed26c92f6ff6d18f34e1d0ed3f
AutomationBoi <himesh8chopra@gmail.com>
2021-06-22 07:41:27 +0000

    further details implemented

--- src/com/locked/me/locked_me.java
+++ src/com/locked/me/locked_me.java
@@ -8,9 +8,19 @@ public class locked_me {
 		while (user_inp!='q') {
 		help_obj.welcome_screen();
 		user_inp=help_obj.get_user_inp();
+		//Use to list files
 		if(user_inp=='l'||user_inp=='L') {
 			System.out.println("List of files present in directory:\n"+help_obj.get_file_names());
 			System.out.println("*************************************************");
+		}
+		//*******************************
+		
+		//To display details
+		if(user_inp=='d'||user_inp=='D') {
+			System.out.flush();  //flush existing stuff on console.
+			help_obj.further_details();
+			System.out.println("*************************************************");
+			user_inp=help_obj.get_user_inp();
 		}
 		}
 	}
--- src/com/locked/me/locked_me_helperfunctions.java
+++ src/com/locked/me/locked_me_helperfunctions.java
@@ -7,9 +7,18 @@ public class locked_me_helperfunctions {
 	public void welcome_screen() {
 		System.out.println("Locked ME File Handler");
 		System.out.println("-dev: Himesh Chopra");
-		System.out.println("Press \"l or L\" to add a file.");
+		System.out.println("Press \"l or L\" to list files.");
 		System.out.println("Press \"d or D\" for further details");
 	}
+	
+	//display further details
+	public void further_details() {
+		System.out.println("Press \"a or A\" to Add a file.");
+		System.out.println("Press \"r or R\" to remove a file.");
+		System.out.println("Press \"s or S\" to search a file.");
+	}
+	
+	
 	//get the key pressed quit if first character pressed is 'q'
 	public char get_user_inp() {
 		Scanner sc= new Scanner(System.in);
