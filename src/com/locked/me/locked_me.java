b49625191f16b28227a9182e22ed55adacdca2e6
AutomationBoi <himesh8chopra@gmail.com>
2021-06-22 07:08:07 +0000

    scnd push

--- src/com/locked/me/locked_me.java
+++ src/com/locked/me/locked_me.java
@@ -2,9 +2,13 @@ package com.locked.me;
 
 public class locked_me {
 	static locked_me_helperfunctions help_obj=new locked_me_helperfunctions();
+	static char user_inp='p';
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		while (user_inp!='q') {
 		help_obj.welcome_screen();
+		user_inp=help_obj.get_user_inp();
+		}
 	}
 
 }
--- src/com/locked/me/locked_me_helperfunctions.java
+++ src/com/locked/me/locked_me_helperfunctions.java
@@ -1,15 +1,20 @@
 package com.locked.me;
 import java.io.*;
-
+import java.util.*;
 
 public class locked_me_helperfunctions {
-	
+	//display welcome screen
 	public void welcome_screen() {
 		System.out.println("Locked ME File Handler");
 		System.out.println("-dev: Himesh Chopra");
 		System.out.println("Press \"l or L\" to add a file.");
 		System.out.println("Press \"d or D\" for further details");
-		System.out.println("Press \"a\" to add a file.");
+	}
+	//get the key pressed quit if first character pressed is 'q'
+	public char get_user_inp() {
+		Scanner sc= new Scanner(System.in);
+		char chr=sc.next().replaceAll("\\s", "").charAt(0);
+		return chr;
 	}
 
 }
