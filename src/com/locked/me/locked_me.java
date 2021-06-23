4c3f484bae666017b8a4d729c48dc683d2ea9a28
AutomationBoi <himesh8chopra@gmail.com>
2021-06-23 19:29:40 +0000

    final push

--- src/com/locked/me/locked_me.java
+++ src/com/locked/me/locked_me.java
@@ -10,7 +10,7 @@ public class locked_me {
 		user_inp=help_obj.get_user_inp();
 		//Use to list files
 		if(user_inp=='l'||user_inp=='L') {
-			System.out.println("List of files present in directory:\n"+help_obj.get_file_names());
+			help_obj.get_file_names();
 			System.out.println("*************************************************");
 		}
 		//*******************************
@@ -21,8 +21,27 @@ public class locked_me {
 			help_obj.further_details();
 			System.out.println("*************************************************");
 			user_inp=help_obj.get_user_inp();
+			if(user_inp=='a'||user_inp=='A') {
+				System.out.println("Please Enter the File name to be added.");
+				String filename=help_obj.get_user_file_name();
+				help_obj.add_file(filename);
+				System.out.println("*************************************************");
+			}
+			if(user_inp=='r'||user_inp=='R') {
+				System.out.println("Please Enter the File name to be Removed.");
+				String filename=help_obj.get_user_file_name();
+				help_obj.remove_file(filename);
+				System.out.println("*************************************************");
+			}
+			if(user_inp=='s'||user_inp=='S') {
+				System.out.println("Please Enter the File name to be searched.");
+				String filename=help_obj.get_user_file_name();
+				help_obj.search_file(filename);
+				System.out.println("*************************************************");
+			}
 		}
 		}
+		System.out.println("Program Succesfully exited.");
 	}
 
 }
--- src/com/locked/me/locked_me_helperfunctions.java
+++ src/com/locked/me/locked_me_helperfunctions.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class locked_me_helperfunctions {
 	//display welcome screen
+	String file_path="./files_dir/";
 	public void welcome_screen() {
 		System.out.println("Locked ME File Handler");
 		System.out.println("-dev: Himesh Chopra");
@@ -16,6 +17,7 @@ public class locked_me_helperfunctions {
 		System.out.println("Press \"a or A\" to Add a file.");
 		System.out.println("Press \"r or R\" to remove a file.");
 		System.out.println("Press \"s or S\" to search a file.");
+		System.out.println("Press \"q or Q\" to quit.");
 	}
 	
 	
@@ -25,20 +27,106 @@ public class locked_me_helperfunctions {
 		char chr=sc.next().replaceAll("\\s", "").charAt(0);
 		return chr;
 	}
-	public List<String> get_file_names() {
+	
+	//check if dir doesnt exist then create
+	public void create_dir() {
+		File dir=new File(file_path);
+		if (!dir.isDirectory()) {
+            dir.mkdir();
+		}
+	}
+	
+	
+	//get file names
+		
+	public String get_user_file_name() {
+		Scanner sc=new Scanner(System.in);
+		String filename=sc.nextLine();
+		return filename;
+	}
+	
+	
+	//List filenames in file_path
+	public void get_file_names() {
 		List<String> results = new ArrayList<String>();
 
-
-		File[] files = new File("./files_dir").listFiles();
+		try {
+		File[] files = new File(file_path).listFiles();
 		//If this pathname does not denote a directory, then listFiles() returns null. 
-
+		
 		for (File file : files) {
 		    if (file.isFile()) {
 		        results.add(file.getName());
 		    }
 		}
 		Collections.sort(results); //sorting in ascending order
-		return results;
+		System.out.println("List of files present in directory "+file_path+":\n"+results);
+		}catch(Exception e){
+			create_dir();
+					}
 	}
+    //Add file to file_path
+	public void add_file(String file_name) {
+    	try {
+            File file = new File(file_path+file_name);
+            file.createNewFile();
+            System.out.println("File added " + file_path+file_name);
+           	
+         } catch(Exception e) {
+        	 create_dir();
+        	 System.out.println("Cannot Create file "+file_path+file_name);
+        	
+         }
+    	
+    }
+	
+	//Remove file to file_path
+	public void remove_file(String file_name) {
+            File file = new File(file_path+file_name);
+            if(file.delete())
+            {
+                System.out.println(file_name+" deleted successfully");
+            }
+            else
+            {
+                System.out.println("Failed to delete "+file_name);
+                create_dir();
+            }
+    	
+    }
+	
+	public void search_file(String file_name) {
+		File dir = new File(file_path);
+		FileFilter filter=new FileFilter(file_name);
+		List<String> results = new ArrayList<String>();
+		try {
+			String[] file_list = dir.list(filter);
+			for (String file : file_list) {
+			      results.add(file);
+			    }
+			System.out.println("List of Files matching name "+file_name+" :\n"+results);
+		}catch(Exception e) {
+			create_dir();
+			System.out.println("Exception occured while searching :\n"+e.toString());
+		}
+	}
+
+	class FileFilter implements FilenameFilter {
+	    
+	    String pattern;
+	    
+	    
+	    public FileFilter(String pattern)
+	    {
+	        this.pattern = pattern;
+	    }
+	    
+	    
+	    public boolean accept(File dir, String name)
+	    {
+	        return name.startsWith(pattern);
+	    }
+	}
+
 
 }
